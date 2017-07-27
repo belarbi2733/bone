96bbfe28ffaac8953371b392efeeaaad8dabcfe5
belarbi2733 <belarbi_mohammed_amin@univ-mosta.dz>
2017-07-27 13:05:01 +0200

    Initial commit

--- symfv2/Enterface16/src/Enterface/ServiceBundle/Controller/ServicesController.php
+++ symfv2/Enterface16/src/Enterface/ServiceBundle/Controller/ServicesController.php
@@ -291,8 +291,11 @@ public function algotreatment1Action()
             }
             
         }
+        $mode=$_POST["cc"];
+        $myfile = fopen($target_dir ."/time.txt", "r");
+        $time_exec=fgets($myfile );
         //return new RedirectResponse($this->container->get('router')->generate('enterface_service_showtracking'));
-        return $this->render('EnterfaceServiceBundle:Services:affichealgo.html.twig', array('works' => true));
+        return $this->render('EnterfaceServiceBundle:Services:affichealgo.html.twig', array('works' => true,'algo'=>$algorithm,'type'=>$typealgo,'mode'=>$mode,'time_exec'=>$time_exec));
     }
     
     public function TiffViewerAction() {
